0a92dca6907af59538ade6ca919a75346cfcad8e
Tim Lee <timlee@Tims-MacBook-Pro-2.local>
2019-01-03 12:40:10 +0000

    Logic controlled by grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class SquareClickable extends Component {
-  render() {
-    return (<td onClick={this.props.onClick}>{this.props.type}</td>);
-  }
-}
-
 class Square extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { type: "Water"}
-    this.handleClick = this.handleClick.bind(this);
-  }
-  
-  handleClick(e) {
-    this.setState({type: "Ship"});
-  }
-
   render() {
-    return <SquareClickable onClick={this.handleClick} type={this.state.type}/>
+    return (<td x={this.props.x} y={this.props.y} onClick={this.props.onClick.bind(null, this.props.x, this.props.y)}>{this.props.displayText}</td>);
   }
 }
 
@@ -29,20 +13,38 @@ class Grid extends Component {
     this.state = {};
     this.state.gridSize = 10;
     this.state.grid = [];
+    let layout = "WSWSSWWWWW";
+    let isDisplayedLookup = "FTTFFFFFFTF";
     for (let i = 0; i < this.state.gridSize; i++) {
       let row = [];
       for (let j = 0; j < this.state.gridSize; j++) {
-        row.push("Water");
+        let isDisplayed = isDisplayedLookup[j] === "T";
+        let displayText = isDisplayed ? layout[j] : "?";
+        row.push({type: layout[j], displayText: displayText, isDisplayed: isDisplayed});
       }
       this.state.grid.push(row);
     }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(x, y) {
+    let grid = this.state.grid;
+    let square = grid[y][x];
+    if (square.isDisplayed) {
+      return;
+    }
+    square.displayText = square.displayText === "W" ? "S" : "W";
+    this.setState({grid: grid});
   }
 
   render() {
     return (
       <table>
         <tbody>
-            {this.state.grid.map((row, i) => <tr key={i}>{row.map((squareType, j) => <Square key={i + ' ' + j}/>)}</tr>)}
+            {this.state.grid.map((row, i) => <tr key={i}>{row.map((squareProps, j) => 
+            <Square x={j} y={i} displayText={squareProps.displayText} onClick={this.handleClick} key={i + ' ' + j}/>)
+            }</tr>)}
         </tbody>
       </table>
     );
